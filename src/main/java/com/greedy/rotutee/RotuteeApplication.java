f88fe46a19c01bb19ddc7084ee52712080498bf5
sanggne kong <7sangne@gmail.com>
2022-04-16 01:57:10 +0900

    init commit

---
+++ src/main/java/com/greedy/rotutee/RotuteeApplication.java
@@ -0,0 +1,13 @@
+package com.greedy.rotutee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotuteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotuteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greedy/rotutee/RotuteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greedy.rotutee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotuteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
