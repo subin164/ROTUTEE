0e881266bdf5ff1c5b9a879a278014d094278f7f
SeoYoung <piggies97@gmail.com>
2022-04-22 15:58:52 +0900

    <feat> : 튜터 대시보드
    <body> : 나의강의, 작성게시물, 알림, 프로필, 통계 차후 완성 예정

---
+++ src/main/java/com/greedy/rotutee/dashboard/lms/dto/LMSNoticeBoardDTO.java
@@ -0,0 +1,12 @@
+package com.greedy.rotutee.dashboard.lms.dto;/** *packageName : com.greedy.rotutee.dashboard.lms.dto
+* fileName : LMSNoticeBoardDTO
+* author : SeoYoung
+* date : 2022-04-22
+* description :
+* ===========================================================
+* DATE AUTHOR NOTE
+* -----------------------------------------------------------
+* 2022-04-22 SeoYoung 최초 생성
+*/
+ public class LMSNoticeBoardDTO {
+}
---
+++ src/main/java/com/greedy/rotutee/dashboard/mypage/dto/tutor/MypageTutorDTO.java
@@ -0,0 +1,12 @@
+package com.greedy.rotutee.dashboard.mypage.dto.tutor;/** *packageName : com.greedy.rotutee.dashboard.mypage.dto.tutor
+* fileName : MypageTutorDTO
+* author : SeoYoung
+* date : 2022-04-22
+* description :
+* ===========================================================
+* DATE AUTHOR NOTE
+* -----------------------------------------------------------
+* 2022-04-22 SeoYoung 최초 생성
+*/
+ public class MypageTutorDTO {
+}
