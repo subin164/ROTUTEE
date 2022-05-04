a87e8a01d018cc7cdd75bdd481a967d41aeab125
DESKTOP-RC8HACN\fuxjo <fuxjohe@gmail.com>
2022-05-04 22:43:51 +0900

    <feat> : 뷰 수정 및 쿠폰 작성
    <body> :  뷰 수정및 쿠폰 작성 중간머지

--- src/main/java/com/greedy/rotutee/coupon/controller/CouponController.java
+++ src/main/java/com/greedy/rotutee/coupon/controller/CouponController.java
@@ -1,31 +1,57 @@
 package com.greedy.rotutee.coupon.controller;
 
 import com.greedy.rotutee.coupon.dto.CouponDTO;
+import com.greedy.rotutee.coupon.service.CouponService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.Date;
+import java.util.List;
+
 @Controller
 @RequestMapping("/coupon")
 public class CouponController {
 
+    private final CouponService couponService;
+
+    @Autowired
+    public CouponController(CouponService couponService) {
+        this.couponService = couponService;
+    }
+
 
     @GetMapping("/list")
-    public String couponList(){
-        return "/coupon/list";
+    public ModelAndView couponList(ModelAndView mv, CouponDTO couponDTO, Pageable pageable) {
+
+        couponService.findCouponList(couponDTO, pageable);
+
+//        System.out.println("couponlist : " + couponList);
+//
+//        mv.addObject("couponList", couponList);
+        mv.setViewName("/coupon/list");
+
+        return mv;
     }
 
     @PostMapping("/regist")
-    public ModelAndView couponRegist(ModelAndView mv, CouponDTO couponDTO){
+    public ModelAndView couponRegist(ModelAndView mv, CouponDTO couponDTO) {
 
         couponDTO.setCouponStatus("N");
-        java.sql.Date expirationDate;
-        System.out.println("couponDTO : " + couponDTO);
+
+        couponDTO.setCouponRegistDate(new Date(System.currentTimeMillis()));
+
+        couponService.couponRegist(couponDTO);
 
         mv.setViewName("/coupon/list");
 
         return mv;
     }
+
 }
